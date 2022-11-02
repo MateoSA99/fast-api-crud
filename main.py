e6e8981bf65c045758efb4941ef4d83e4e1830b4
Mateo <mateo.sotoa@udea.edu.co>
2022-11-01 23:12:09 -0500

    Validations with pydantic

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+
 from fastapi import FastAPI
+
+from app.v1.router.user_router import router as user_router
 
 app = FastAPI()
 
-@app.get("/")
-async def home():
-    return {"message": "Probando FastAPI"}
+app.include_router(user_router)
